e0af0fb9cd97c4e0b01e6c7923d69962ea499b0c
aha <aha@scireum.de>
2015-12-03 22:23:18 +0100

    Adds an empty test to make maven happy
    Git irgnores empty directories which fails the maven build...

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,13 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+/**
+ * Created by aha on 03.12.15.
+ */
+public class DummyTest {
+}
